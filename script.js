e547bba7fdfd3fa23cea5e5294015f2e87e28edd
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-30 20:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-async function showMessageWithDelay() {
-  const textInput = document.getElementById('text');
-  const delayInput = document.getElementById('delay');
-  const outputDiv = document.getElementById('output');
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
 
+// Add click event listener to the button
+btn.addEventListener('click', showMessage);
+
+// Function to display the message after a delay
+async function showMessage() {
   const text = textInput.value;
-  const delay = parseInt(delayInput.value);
+  const delay = parseInt(delayInput.value, 10);
 
-  if (text && !isNaN(delay)) {
-    await delayInMilliseconds(delay);
-    outputDiv.innerText = text;
+  // Validate input
+  if (text.trim() === '' || isNaN(delay) || delay <= 0) {
+    outputDiv.textContent = 'Invalid input';
+    return;
   }
-}
 
-function delayInMilliseconds(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  // Delay using a Promise
+  function delayPromise(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  // Wait for the specified delay
+  await delayPromise(delay);
+
+  // Display the message
+  outputDiv.textContent = text;
 }
