6c9087a1e3239b4865142da3be3e36a1a61e4670
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-30 18:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let text = document.getElementById('text').value;
+let delayValue = document.getElementById('delay').value;
+let delay = parseInt(delayValue);
+
+async function callme(){
+	// alert(delay);
+	let value = await new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(text);
+		},1000)
+	})
+	let div = document.getElementById('output');
+	div.innerText = value;
+}
+
+// alert(text);
+
+
+
